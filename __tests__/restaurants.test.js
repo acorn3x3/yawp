a0bcc9c7cea63cd677408ea657e09f82a6062d1e
Andrew Cornejo <atcornejo3@gmail.com>
2022-11-30 11:41:37 -0800

    fixed a type

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -4,7 +4,6 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
-
 const mockUser = {
   firstName: 'Test',
   lastName: 'User',
@@ -123,14 +122,14 @@ describe('restaurant routes;', () => {
       }
     `);
   });
-//   it('DELETE /api/v1/reviews/:id should delete the review if request is made by reviewer or admin', async () => {
-//     const [agent] = await registerAndLogin();
-//     await agent
-//       .post('/ap1/v1/restaurants/1/reviews')
-//       .send({ stars: 20, detail: 'wow, I live at this foodery now' });
-//     const resp = await agent.delete('/api/v1/reviews/4');
-//     expect(resp.status).toBe(204);
-//     const deleteResp = await agent.get('/api/v1/reviews/4');
-//     expect(deleteResp.status).toBe(404);
-//   });
-// });
+  //   it('DELETE /api/v1/reviews/:id should delete the review if request is made by reviewer or admin', async () => {
+  //     const [agent] = await registerAndLogin();
+  //     await agent
+  //       .post('/ap1/v1/restaurants/1/reviews')
+  //       .send({ stars: 20, detail: 'wow, I live at this foodery now' });
+  //     const resp = await agent.delete('/api/v1/reviews/4');
+  //     expect(resp.status).toBe(204);
+  //     const deleteResp = await agent.get('/api/v1/reviews/4');
+  //     expect(deleteResp.status).toBe(404);
+  //   });
+});
